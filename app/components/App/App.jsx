8f55c61077518b3595902eeabcfa4a931c6da757
Jennifer Woodson <jenniferpwoodson@gmail.com>
2017-10-18 15:50:00 -0600

    implement thunk and redux

--- app/components/App/App.jsx
+++ app/components/App/App.jsx
@@ -19,7 +19,7 @@ export default class App extends Component {
 
   componentDidMount() {
     console.log('app props: ', this.props);
-    getNowPlaying()
+    this.props.getMovies()
   }
 
   render() {
--- app/index.js
+++ app/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import AppContainer from './containers/AppContainer.js';
 import MovieIndex from './components/movieIndex';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers';
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-const store = createStore(rootReducer, devTools)
+const store = createStore(rootReducer, devTools, applyMiddleware(thunk))
 
 
 ReactDOM.render(
