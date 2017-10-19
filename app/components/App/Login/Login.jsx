5679842aec6c7cb4cb6abfebbc32d36a711b1b74
Jennifer Woodson <jenniferpwoodson@gmail.com>
2017-10-19 14:08:57 -0600

    update Login state:

--- app/components/App/Login/Login.jsx
+++ app/components/App/Login/Login.jsx
@@ -4,16 +4,24 @@ class Login extends Component {
   constructor() {
     super()
     this.state = {
-
+      name: '',
+      email: '',
+      password: ''
     }
+  }
+
+  updateState(event, key) {
+    this.setState({
+      [key]: event.target.value
+    });
   }
 
   render() {
     return (
       <div className='login'>
-        <input type="text" placeholder="name"/>
-        <input type="text" placeholder="email"/>
-        <input type="text" placeholder="password"/>
+        <input type="text" placeholder="name" onChange={(event) => this.updateState(event, 'name')}/>
+        <input type="text" placeholder="email" onChange={(event) => this.updateState(event, 'email')}/>
+        <input type="text" placeholder="password" onChange={(event) => this.updateState(event, 'password')}/>
         <button>Submit</button>
       </div>
     )
