721e69be3c1746130cc66ca48e628d64c982851e
Jennifer Woodson <jenniferpwoodson@gmail.com>
2017-10-19 13:43:32 -0600

    add login route

--- app/components/App/App.jsx
+++ app/components/App/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { getNowPlaying, getMovies } from '../../actions';
 import { BrowserRouter, Route } from 'react-router-dom';
 import CardCatalogContainer from './../../containers/CardCatalogContainer.js';
+import { Switch } from 'react-router';
 
 
 export default class App extends Component {
@@ -13,15 +14,25 @@ export default class App extends Component {
     return (
       <BrowserRouter>
         <div className='app'>
-          <Route path='/' render={() => {
-            return (
-              <div>
-                <button>Login</button>
-                <CardCatalogContainer />
-              </div>
-            )
-          }} />
-        </div>
+          <Switch>
+
+            <Route path='/' render={() => {
+              return (
+                <div>
+                  <button>Login</button>
+                  <CardCatalogContainer />
+                </div>
+              )
+            }} />
+
+            <Route path='/login' render={() =>
+              <h2>
+                LOGIN
+              </h2>
+            } />
+          </Switch>
+
+      </div>
       </BrowserRouter>
     )
   }
