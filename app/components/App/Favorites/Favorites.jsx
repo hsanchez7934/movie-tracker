bc5f5a962192e167f7b656baec218ebc6fe7dbf2
Jason Hughes <jasonhughes088@gmail.com>
2017-10-22 13:02:57 -0600

    favorites route is fully functional and displaying our favorite cards

--- app/components/App/Favorites/Favorites.jsx
+++ app/components/App/Favorites/Favorites.jsx
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
+import MovieCard from './../CardCatalog/MovieCard/MovieCard';
 
-const Favorites = () => {
-  return (
-    <h2>Favorites!</h2>
+const Favorites = (props) => {
+  const { favorites , addFavorite, login } = props;
+  const mappedCards = favorites.map( (movie, index) => {
+    const { title, release_date, poster_path, vote_average, overview, movie_id } = movie;
+    return ( <MovieCard
+      key={index}
+      title={title}
+      releaseDate={release_date}
+      image={poster_path}
+      addFavorite={addFavorite}
+      voteAverage={vote_average}
+      overview={overview}
+      movieID={movie_id}
+      login={login}
+      movie={movie}
+    />)
+
+  })
+      return (
+        <div className="card-catalog">
+          {mappedCards}
+        </div>
   )
 }
 
