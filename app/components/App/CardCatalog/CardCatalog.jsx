d4f4b7161ea377bfc4b83696dcd3206f2038b8e9
Jason Hughes <jasonhughes088@gmail.com>
2017-10-23 08:08:31 -0600

    add additional styling and snapshot for app test

--- app/components/App/CardCatalog/CardCatalog.jsx
+++ app/components/App/CardCatalog/CardCatalog.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import MovieCardContainer from './../../../containers/MovieCardContainer';
+import css from './MovieCard/MovieCard.scss';
+
 
 
 export default class CardCatalog extends Component {
   constructor() {
     super();
   }
+
+  activeClass(movie, favoritesDB) {
+  const cardIndex = favoritesDB.findIndex(
+    (movieObject) => movieObject.movie_id === movie.id
+  );
+  return cardIndex !== -1 ? '.fav-button' : '.remove-fav';
+};
 
   componentDidMount() {
     this.props.getMovies()
@@ -14,10 +23,12 @@ export default class CardCatalog extends Component {
   render() {
     const { movies, addFavorite, login, deleteFavorite, favoritesDB } = this.props;
     const mappedCards = movies.map( (movie, index) => {
-      // console.log('catalog-move: ', movie);
+      console.log('movie: ', movie);
       return (
         <MovieCardContainer movie={movie}
-                            key={index} />
+                            key={index}
+                            activeClass={this.activeClass(movie, favoritesDB)}
+                          />
       )
     })
     return (
--- app/components/App/CardCatalog/MovieCard/MovieCard.jsx
+++ app/components/App/CardCatalog/MovieCard/MovieCard.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 
 const favoriteMovie = (id, movie) => {
   const { title, release_date, poster_path, vote_average, overview } = movie;
+
+
+
   return {  user_id: id,
             title,
             release_date,
@@ -23,16 +26,16 @@ const handleClick = (id, movie, favoritesDB, deleteFavorite, addFavorite) => {
          : addFavorite(favoriteMovie(id, movie));
 }
 
-const MovieCard = ({ addFavorite, login, movie, deleteFavorite, favoritesDB }) => {
+const MovieCard = ({ addFavorite, login, movie, deleteFavorite, favoritesDB, activeClass }) => {
 const { title, release_date, poster_path, vote_average, overview } = movie;
+
   return(
     <article className='movie-card'>
-      <button className="fav-button" onClick={() => handleClick(login.id, movie, favoritesDB, deleteFavorite, addFavorite)}>Add Favorite</button>
+      <button className={activeClass} onClick={() => handleClick(login.id, movie, favoritesDB, deleteFavorite, addFavorite)}>Add Favorite</button>
       <div className="header-container">
         <h3 className="title-card">{title}</h3>
         <p className="release-date-card">Release Date: {release_date}</p>
-      <p className="overview">Overview: {overview}</p>
-      <p className="voteAverage">Rating: {vote_average}</p>
+      <p className="vote-average">Rating: {vote_average}</p>
     </div>
       <img className="movie-image" src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={`${title} - poster`} />
     </article>
