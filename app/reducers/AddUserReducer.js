f544960e0a83d1bd5880e1d557f2a455763bf0a7
Jennifer Woodson <jenniferpwoodson@gmail.com>
2017-10-23 00:39:43 -0600

    remove comment and change ADD_USER payload to object instead of array

--- app/reducers/AddUserReducer.js
+++ app/reducers/AddUserReducer.js
@@ -1,7 +1,7 @@
-const user = (state = [], action) => {
+const user = (state = {}, action) => {
   switch (action.type) {
     case "ADD_USER":
-      return [...state, action.user]
+      return action.user
     default:
       return state;
   }
